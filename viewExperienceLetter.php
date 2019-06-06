9db5e841122cf83a4c840248a564f514d481a7cd
logaprakash <logaprakash@dataphilabs.com>
2019-06-06 12:37:39 +0530

    php removed

--- viewExperienceLetter.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Length: " . filesize ('android_intern.pdf') ); 
-header("Content-type: application/pdf"); 
-header("Content-disposition: inline;     
-filename=".basename('android_intern.pdf'));
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-$filepath = readfile('android_intern.pdf');
-?>
